3cfbd1ebc4f7144a2e370dbaa21c80d009cbf161
almakvim <168618048+almakvim@users.noreply.github.com>
2024-05-03 09:09:33 +0300

    Add files via upload

---
+++ MDK-ARM/RTE/_RF_103_RELE_2/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RF_103_RELE_2' 
+ * Target:  'RF_103_RELE_2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
